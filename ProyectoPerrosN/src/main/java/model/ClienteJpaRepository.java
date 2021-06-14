0232472f2e27503230911ba297dfbcc53f47f7bc
JulianaGoyeneche <jgoyeneche42@misena.edu.co>
2021-06-13 20:45:07 -0500

    Carpeta proyecto Perros Nacho

---
+++ ProyectoPerrosN/src/main/java/model/ClienteJpaRepository.java
@@ -0,0 +1,9 @@
+package model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface ClienteJpaRepository extends JpaRepository<Cliente,Integer>{
+
+    
+}
